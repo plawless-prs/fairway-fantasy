5cc1e9a797daed7c79892d925e64054de2dcca30
unknown <Porter Lawless@11-PLAWLESS.prs.local>
2026-04-10 09:07:55 -0500

    add persistsession line in supabase page

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -11,4 +11,9 @@ if (!supabaseUrl || !supabaseAnonKey) {
   );
 }
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    persistSession: true,
+    autoRefreshToken: true,
+  },
+});
